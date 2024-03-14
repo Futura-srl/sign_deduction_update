46dafbd9cf544f211e28e3401298bb3d312748d4
Luca-Cocozza <luca.cocozza@futurasl.com>
2024-03-14 09:55:44 +0100

    Add salvataggio user e datetime di chi mette check sugli addebiti effettuati

--- models/deduction_deduction.py
+++ models/deduction_deduction.py
@@ -3,5 +3,16 @@ from odoo import models, fields, api
 
 class DeductionDeduction(models.Model):
     _inherit = 'deduction.deduction'
-   
+    
     processed = fields.Boolean()
+    processed_by = fields.Char(readonly=True)
+    processed_on = fields.Datetime(readonly=True)
+    
+    @api.onchange('processed')
+    def _deduction_processed(self):
+        if self.processed:
+            self.processed_by = self.env.user.name
+            self.processed_on = fields.Datetime.now()
+        else:
+            self.processed_by = False
+            self.processed_on = False
