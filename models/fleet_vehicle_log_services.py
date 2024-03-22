0b7acb98818b2d277bc68a14f6158e93549d1da7
Luca-Cocozza <luca.cocozza@futurasl.com>
2024-03-22 11:38:54 +0100

    Aggiunta campo competence_invoice nei log_services

--- models/fleet_vehicle_log_services.py
+++ models/fleet_vehicle_log_services.py
@@ -30,6 +30,7 @@ class FleetVehicleLogServices(models.Model):
     replacement_start_date = fields.Datetime(string="Replacement Start Datetime")
     replacement_end_date = fields.Datetime(string="Replacement Start Datetime")
     email_ids = fields.One2many('mail.mail', 'res_id', string='Emails', domain="[('model','=', 'fleet.vehicle.log.services'), ('res_id', '=', id)]")
+    competence_invoice = fields.Char()
 
     @api.model_create_multi
     def create(self, vals_list):
