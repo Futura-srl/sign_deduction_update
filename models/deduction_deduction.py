2da4bdae514737672b42e564f006259c3d1173fc
Luca Cocozza <luca.cocozza@futurasl.com>
2025-08-18 09:30:51 +0200

    Update

--- models/deduction_deduction.py
+++ models/deduction_deduction.py
@@ -1,73 +1,166 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+import logging, requests, json, xml.etree.ElementTree as ET
+from zoneinfo import ZoneInfo
+from datetime import datetime
+
+
+_logger = logging.getLogger(__name__)
 
 
 class DeductionDeduction(models.Model):
     _inherit = 'deduction.deduction'
     
     processed = fields.Boolean()
-    processed_by = fields.Char(readonly=True)
-    processed_on = fields.Datetime(readonly=True)
+    processed_by = fields.Many2one('res.users', string='Processed By', help="User who processed the deduction.", default=False)
+    processed_on = fields.Datetime()
     invoice = fields.Char()
     on_pwork = fields.Boolean(default=False, help="Check this box if the deduction is uploaded to Pwork.")
-    pwork_error = fields.Text(string='Pwork Error', help="Error message if the upload to Pwork fails.")
+    response_txt = fields.Text(string='Pwork Response', help="Response from Pwork after processing the deduction.")
+    payload = fields.Text(string='Payload', help="The XML payload sent to Pwork.")
+    error = fields.Boolean(default=False, help="Indicates if there was an error during processing.")
     
-    @api.onchange('processed')
-    def _deduction_processed(self):
-        if self.processed:
-            self.processed_by = self.env.user.name
-            self.processed_on = fields.Datetime.now()
-        else:
-            self.processed_by = False
-            self.processed_on = False
 
-
-    def action_upload_to_pwork(self):
+    def action_deduction_processed(self):
         for record in self:
-            if not record.invoice:
-                record.pwork_error = "Invoice number is required to upload to Pwork."
-                return False
+            if not record.employee_id.is_company:
+                raise UserError(_("The employee must not be a company to process the deduction. Please use the action upload deduction to pwork method instead."))
+            if not record.processed:
+                record.processed = True
+                record.processed_by = record.env.user.name
+                record.processed_on = fields.Datetime.now()
+            else:
+                record.processed = False
+                record.processed_by = False
+                record.processed_on = False
+
+
+    def action_upload_deduction_to_pwork(self):
+        for record in self:
+            _logger.info(f"Processing deduction id {record.id} for partner {record.employee_id.name}.")
+            if not record.on_pwork:
+                try:
+                    _logger.info(f"Uploading deduction for employee {record.employee_id.name} to Pwork.")
+                    # Chiamata alla funzione per l'upload su Pwork
+                    payload, response = record.setTM_Voce()
+                    record.payload = payload
+                    record.response_txt = response
+                    if response['ckResponse']['Esito'] == 2:
+                        record.on_pwork = True
+                        record.error = False
+                        record.processed = True
+                        record.processed_by = self.env.user
+                        record.processed_on =  datetime.now()
+                        _logger.info(f"Deduction for employee {record.employee_id.name} successfully uploaded to Pwork.")
+                    else:
+                        record.error = True
+                except Exception as e:
+                    record.error = True
+                    record.response_txt = str(e)
+                    _logger.error(f"Exception occurred while uploading deduction for employee {record.employee_id.name}: {record.response_txt}")
+            else:
+                _logger.info(f"Deduction for employee {record.employee_id.name} is already on Pwork, skipping upload.")
+                continue
 
 
 
-            # Simulate the upload process
-            try:
-                # Here you would implement the actual upload logic to Pwork
-                # For now, we just simulate a successful upload
-                record.on_pwork = True
-                record.pwork_error = False
-            except Exception as e:
-                record.pwork_error = str(e)
-                return False
 
-        return True
+    def _get_access_data(self):
+        config_obj = self.env['ir.config_parameter']
+        pwork_cod_azienda = config_obj.sudo().get_param('pwork_cod_azienda')
+        pwork_token = config_obj.sudo().get_param('pwork_token')
+        return config_obj, pwork_cod_azienda, pwork_token
 
-    def upload_voce_pwork(self, token, cod_azienda, codice_fiscale, data, voce, value, qta):
-        data_xml = """
-                url = 'https://futura.presenze-online.it/webservice/ws.asmx'
-                headers = {
-                    'Content-Type': 'application/soap+xml; charset=utf-8',
-                }
+    def setTM_Voce(self):
+        rome_tz = ZoneInfo("Europe/Rome")
+        config_obj, pwork_cod_azienda, pwork_token = self._get_access_data()
+        _logger.info("Avvio connessione")
+
+        partner_id = self.employee_id
+        date = self.date
+        # Controlloo se il partner e` un azienda e nel caso salto il record
+        if partner_id.is_company:
+            return False
+        # Controllo se il partner ha dipendenti attivi nella data
+        employees = self.env['hr.employee'].search([('address_home_id', '=', partner_id.id), ('active', 'in', [True, False])])
+        # Cerco il dipendete con contratti attivi nella data
+        if not employees:
+            raise UserError(_("No employees found for this partner."))
+        for employee in employees:
+            contract = self.env['hr.contract'].search([('employee_id', '=', employee.id), ('date_start', '<=', date), '|', ('date_end', '>=', date), ('date_end', '=', False)])
+            if contract:
+                # Se ho trovato un contratto salvo l'id del dipendente ed esco dal ciclo e procedo con la chiamata
+                employee_id = employee.id
+                break
+        if not contract:
+            raise UserError(_("No active contract found for employee {} on date {}.".format(employee.name, date)))
 
 
-                # costruzione del payload della richiesta SOAP XML
-                payload = '''<?xml version="1.0" encoding="utf-8"?>
-            <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-                <soap12:Body>
-                    <setTM_Voce xmlns="https://presenze-online.it/">
-                        <Token>{}</Token>
-                        <CodAzienda>{}</CodAzienda>
-                        <Params>
-                            <RequestTMVociEx>
-                                <CodiceFiscale>{}</CodiceFiscale>
-                                <Data>{}</Data>
-                                <Voce>{}</Voce>
-                                <Value>{}</Value>
-                                <Qta>{}</Qta>
-                            </RequestTMVociEx>
-                        </Params>
-                        <ReturnType>FormatJson</ReturnType>
-                    </setTM_Voce>
-                </soap12:Body>
-            </soap12:Envelope>'''.format(token, cod_azienda, codice_fiscale, data, voce, value, qta))
 
-                """
+        # Recupero i dati per la chiamata
+        employee = self.env['hr.employee'].browse(employee_id)
+        pwork_azienda_id = employee.pwork_azienda_id
+        pwork_dipendente_id = employee.pwork_dipendente_id
+        # trasformo la data in formato dd/mm/yyyy e metto la timezone di Roma
+        date = (date.astimezone(rome_tz)).strftime("%d/%m/%Y")
+        # Recupero il tipo di voce
+        service_id = self.fleet_vehicle_log_service_id.service_type_id.id
+        value = self.deduction_value
+
+
+        if service_id == 80: # Multe
+            voce = "ADDCO"
+        elif service_id == 81: # Sinistri
+            voce = "ADDDA"
+        else:
+            raise UserError(_("Service type not supported for deduction upload to Pwork."))
+
+
+        url = 'https://futura.presenze-online.it/webservice/ws.asmx'
+        headers = {
+            'Content-Type': 'application/soap+xml; charset=utf-8',
+        }
+
+        # costruzione del payload della richiesta SOAP XML
+        payload = '''<?xml version="1.0" encoding="utf-8"?>
+                    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+                        <soap12:Body>
+                            <setTM_Voce xmlns="https://presenze-online.it/">
+                                <Token>{}</Token>
+                                <CodAzienda>{}</CodAzienda>
+                                <Params>
+                                    <RequestTMVociEx>
+                                        <idDip>{}</idDip>
+                                        <idAzienda>{}</idAzienda>
+                                        <Data>{}</Data>
+                                        <Voce>{}</Voce>
+                                        <Value>{}</Value>
+                                    </RequestTMVociEx>
+                                </Params>
+                                <ReturnType>FormatJson</ReturnType>
+                            </setTM_Voce>
+                        </soap12:Body>
+                    </soap12:Envelope>'''.format(pwork_token, pwork_cod_azienda, pwork_dipendente_id, pwork_azienda_id, date, voce, value)
+
+        _logger.info(payload)
+        _logger.info("Invio della richiesta HTTP POST")
+
+        # Invio della richiesta HTTP POST
+        response = requests.post(url, headers=headers, data=payload)
+
+        _logger.info("Stampa dello stato della risposta HTTP e del contenuto della risposta")
+
+        # stampa dello stato della risposta HTTP e del contenuto della risposta
+        _logger.info(response)
+        _logger.info(response.status_code)
+        _logger.info(response.content)
+
+        # Analisi del documento XML
+        xml_string = response.content
+        root = ET.fromstring(xml_string)
+
+        # Recupero del valore della stringa JSON
+        result = root.find('.//{https://presenze-online.it/}setTM_VoceResult').text.strip()
+        data = json.loads(result)
+        _logger.info(data)
+        return payload, data
